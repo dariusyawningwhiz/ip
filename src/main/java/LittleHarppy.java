e3326b2b82fcfef93a52606c5e42e7d84247717c
dariusyawningwhiz <e1155278@u.nus.edu>
2025-01-24 00:59:31 +0800

    change

--- src/main/java/LittleHarppy.java
+++ src/main/java/LittleHarppy.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class LittleHarpy {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -7,4 +7,9 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
     }
+    String chatbotName = "LittleHarpy"
+    //Hello! I'm little.Harpy
+
+    //What can I do for you?
+    //Bye. Hope to see you again soon!
 }
